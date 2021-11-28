f8ff41f3616fa15d84e5c8358eaf4c8b20678267
ZasminEvgrafenya <95141239+ZasminEvgrafenya@users.noreply.github.com>
2021-11-28 09:41:07 +0300

    Update script.js

--- API_example/script.js
+++ API_example/script.js
@@ -2,9 +2,9 @@ let qrcd;
 
 function initMap() {
   var qrcd = new POST(document.getElementById('qr'), {
-    "frame_name": "no-frame",
-    "qr_code_text": "https://www.qr-code-generator.com/",
-    "image_format": "SVG",
-    "qr_code_logo": "scan-me-square"
+    'frame_name': 'no-frame',
+    'qr_code_text': 'https://www.qr-code-generator.com/',
+    'image_format': 'SVG',
+    'qr_code_logo': 'scan-me-square'
   });
 }
